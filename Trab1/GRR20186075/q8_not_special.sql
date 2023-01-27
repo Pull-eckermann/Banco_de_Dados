14b92f74acfc498c85da5ddf013421f4c60a405a
Pull-eckermann <eric7880@gmail.com>
2023-01-26 21:38:56 -0300

    .

--- Trab1/GRR20186075/q8_not_special.sql
+++
@@ -1,9 +0,0 @@
-select count(*)
-from(
-    select distinct O_CUSTKEY
-    from ORDERS
-    except
-    select distinct O_CUSTKEY               
-    from ORDERS
-    where O_COMMENT like '%special request%'
-);
